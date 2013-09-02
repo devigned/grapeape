8d632ed7e8f74659bfeac5793dcd252216c49aef
David Justice <david@devigned.com>
2013-09-02 11:42:01 -0400

    add in a few basic specs to the api

--- lib/grape_ape/api.rb
+++ lib/grape_ape/api.rb
@@ -4,35 +4,37 @@ module GrapeApe
 
     REQUIRED_ROUTE_KEYS = [:worker, :method]
 
-    def route(methods, paths = %w(/), route_options = {}, &block)
-      if ape_route?(route_options)
-        endpoint_options = {
-            :method => methods,
-            :path => paths,
-            :route_options => (@namespace_description || {}).deep_merge(@last_description || {}).deep_merge(route_options || {})
-        }
+     class << self
+      def route(methods, paths = %w(/), route_options = {}, &block)
+        if ape_route?(route_options)
+          endpoint_options = {
+              :method => methods,
+              :path => paths,
+              :route_options => (@namespace_description || {}).deep_merge(@last_description || {}).deep_merge(route_options || {})
+          }
 
-        endpoints << Grape::Endpoint.new(settings.clone, endpoint_options) do
-          message = rpc(env, route_options[:worker], {method: route_options[:method], params: params})
-          if block
-            block.call(message)
-          else
-            message
+          endpoints << Grape::Endpoint.new(settings.clone, endpoint_options) do
+            message = rpc(env, route_options[:worker], {method: route_options[:method], params: params})
+            if block
+              block.call(message)
+            else
+              message
+            end
           end
+
+          @last_description = nil
+          reset_validations!
+        else
+          super
         end
-
-        @last_description = nil
-        reset_validations!
-      else
-        super
       end
-    end
 
-    private
+      private
 
-    def ape_route?(route_options)
-      keys = Set.new(route_options.keys)
-      Set[*REQUIRED_ROUTE_KEYS].subset?(keys)
+      def ape_route?(route_options)
+        keys = Set.new(route_options.keys)
+        Set[*REQUIRED_ROUTE_KEYS].subset?(keys)
+      end
     end
   end
 end
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -2,6 +2,22 @@ require 'spec_helper'
 
 describe GrapeApe::API do
   context '#route' do
+    let(:ape_options) { {worker: 'blah', method: 'hello'} }
 
+    it 'should ask if the route is an ape route' do
+      described_class.should_receive(:ape_route?).with(hash_including(ape_options)).and_return(true)
+      described_class.route(:get, '/', ape_options)
+    end
+
+    it 'should call super route with no ape route options' do
+      described_class.superclass.should_receive(:route)
+      described_class.route(:get, '/')
+    end
+
+    it 'should add an endpoint to the endpoint collection' do
+      expect {
+        described_class.route(:get, '/', ape_options)
+      }.to change { described_class.endpoints.count }.from(1).to(2)
+    end
   end
 end
