986352081bd515ea71cec3d8b9a4506219ee2356
David Justice <david@devigned.com>
2013-09-14 11:24:18 -0500

    reword the tests and add 404

--- spec/test_helper_spec.rb
+++ spec/test_helper_spec.rb
@@ -12,18 +12,28 @@ describe GrapeApe::TestHelper do
     end
   end
 
-  it 'should make a request to root and get a 200' do
-    with_api(TestApi) do
-      em_http_client = get_request path: '/'
-      em_http_client.response_header.status.should eq(200)
+  context 'make request to root' do
+    it 'should get a 200' do
+      with_api(TestApi) do
+        em_http_client = get_request path: '/'
+        em_http_client.response_header.status.should eq(200)
+      end
+    end
+
+    it 'should get json' do
+      with_api(TestApi) do
+        em_http_client = get_request path: '/'
+        JSON.parse(em_http_client.response).should eq({'hello' => 'world'})
+      end
     end
   end
 
-  it 'should make a request to root and get a 200' do
-    with_api(TestApi) do
-      em_http_client = get_request path: '/'
-      JSON.parse(em_http_client.response).should eq({'hello' => 'world'})
+  context 'make request to not routed path' do
+    it 'get a 404' do
+      with_api(TestApi) do
+        em_http_client = get_request path: '/something'
+        em_http_client.response_header.status.should eq(404)
+      end
     end
   end
-
 end
